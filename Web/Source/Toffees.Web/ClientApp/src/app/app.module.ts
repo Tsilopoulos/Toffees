7224ad99743ebd6b22a564b281479de4e5417d62
Panagiotis Tsilopoulos <pan_tsilopoulos@outlook.com>
2018-03-18 01:06:28 +0200

    Add ngD3 for data visualization in the dashboard route

--- Web/Source/Toffees.Web/ClientApp/src/app/app.module.ts
+++ Web/Source/Toffees.Web/ClientApp/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgxChartsModule } from "@swimlane/ngx-charts";
 
 import { AuthHttpInterceptor } from "./auth/auth.interceptor";
 import { AuthenticationService } from "./auth/authentication.service";
@@ -17,6 +19,7 @@ import { GlucoseComponent } from "./glucose/glucose.component";
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { ConfirmationDialogComponent } from "./confirmation-dialog/confirmation-dialog.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
   declarations: [
@@ -26,17 +29,21 @@ import { ConfirmationDialogComponent } from "./confirmation-dialog/confirmation-
     GlucoseComponent,
     RegisterComponent,
     LoginComponent,
-    ConfirmationDialogComponent
+    ConfirmationDialogComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     NgbModule.forRoot(),
+    NgxChartsModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent, pathMatch: "full" },
       { path: "glucose", component: GlucoseComponent },
+      { path: "dashboard", component: DashboardComponent },
       { path: "login", component: LoginComponent },
       { path: "register", component: RegisterComponent }
     ])
