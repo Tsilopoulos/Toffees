445042e325613a2da473e1db26dd048d29e82405
Panagiotis Tsilopoulos <pan_tsilopoulos@outlook.com>
2018-03-19 00:09:59 +0200

    Add generic BG line chart in dashboard

--- Web/Source/Toffees.Web/ClientApp/src/app/dashboard/dashboard.component.ts
+++ Web/Source/Toffees.Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -1,56 +1,60 @@
-import { Component, OnInit } from "@angular/core";
-import {single, multi} from "./data";
+import { Component, OnInit, Inject } from "@angular/core";
+import { NgIf } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { GlucoseService } from "../glucose/glucose.service";
+import { IGlucose } from "../glucose/glucose.component";
 
-// @Component({
-//   selector: "app-dashboard",
-//   templateUrl: "./dashboard.component.html",
-//   styleUrls: ["./dashboard.component.css"]
-// })
 @Component({
   selector: "app-dashboard",
-  template: `
-    <ngx-charts-line-chart
-      [view]="view"
-      [scheme]="colorScheme"
-      [results]="multi"
-      [gradient]="gradient"
-      [xAxis]="showXAxis"
-      [yAxis]="showYAxis"
-      [legend]="showLegend"
-      [showXAxisLabel]="showXAxisLabel"
-      [showYAxisLabel]="showYAxisLabel"
-      [xAxisLabel]="xAxisLabel"
-      [yAxisLabel]="yAxisLabel"
-      [autoScale]="autoScale"
-      (select)="onSelect($event)">
-    </ngx-charts-line-chart>
-  `
+  styleUrls: ["./dashboard.component.css"],
+  templateUrl: "./dashboard.component.html"
 })
 export class DashboardComponent {
 
-  single: any[];
-  multi: any[];
+  series: any[];
+  dataLoaded: boolean;
   view: any[] = [700, 400];
 
-  // options
+  // ngx-charts options
   showXAxis = true;
   showYAxis = true;
   gradient = false;
   showLegend = true;
   showXAxisLabel = true;
-  xAxisLabel = "Country";
+  xAxisLabel = "";
   showYAxisLabel = true;
-  yAxisLabel = "Population";
-
+  yAxisLabel = "mg/dL";
+  timeline = true;
+  autoScale = false;
+  showRefLines = true;
+  showRefLabels = true;
+  referenceLines = [
+    { value: 180, name: "High" },
+    { value: 80, name: "Normal" },
+    { value: 40, name: "Low" }
+  ];
   colorScheme = {
     domain: ["#5AA454", "#A10A28", "#C7B42C", "#AAAAAA"]
   };
 
-  // line, area
-  autoScale = true;
+  constructor(private readonly glucoseService: GlucoseService,
+    private readonly http: HttpClient,
+    @Inject("API_GATEWAY_URL") apiGatewayUrl: string) {
+      this.dataLoaded = false;
+      http.get<IGlucose[]>(apiGatewayUrl + `api/glucose/${localStorage.getItem("userId")}`).subscribe(result => {
+        const series = [{ "name": "BG", "series": this.transform(result) }];
+        Object.assign(this, { series });
+        this.dataLoaded = true;
+      }, error => console.error(error));
+  }
 
-  constructor() {
-    Object.assign(this, {single, multi});
+  transform(glucoses: IGlucose[]): Object[] {
+    const series = [];
+    glucoses.forEach(function (glucose) {
+      const bgDataPoint = { "name": glucose.pinchDateTime, "value": glucose.data };
+      series.push(bgDataPoint);
+    });
+    return series;
   }
 
   onSelect(event) {
