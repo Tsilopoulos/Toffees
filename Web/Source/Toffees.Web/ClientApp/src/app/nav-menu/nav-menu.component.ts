41150d492cc831779170b81248d5655f111933b6
Panagiotis Tsilopoulos <pan_tsilopoulos@outlook.com>
2018-03-20 00:26:01 +0200

    Updated bottom navbar for optimal mobile navigation using ng-bootstrap

--- Web/Source/Toffees.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Web/Source/Toffees.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,54 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
+import { trigger, state, style, animate, transition } from "@angular/animations";
 
 @Component({
   selector: "app-nav-menu",
   templateUrl: "./nav-menu.component.html",
-  styleUrls: ["./nav-menu.component.css"]
+  styleUrls: ["./nav-menu.component.css"],
+  animations: [
+    trigger("collapse", [
+      state("open", style({
+        opacity: "1"
+      })),
+      state("closed", style({
+        opacity: "0",
+        display: "none",
+      })),
+      transition("closed => open", animate("400ms ease-in")),
+      transition("open => closed", animate("100ms ease-out"))
+    ])
+  ]
 })
-export class NavMenuComponent {
-  isExpanded = false;
+export class NavMenuComponent implements OnInit {
 
-  collapse() {
-    this.isExpanded = false;
+  isNavbarCollapsed = true;
+  _isNavbarCollapsedAnim = "closed";
+
+  ngOnInit() {
+    this.onResize(window);
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  @HostListener("window:resize", ["$event.target"])
+  onResize(event) {
+    if (event.innerWidth > 990) {
+      // need to set this to 'open' for large screens to show up because of opacity in 'closed' animation.
+      this._isNavbarCollapsedAnim = "open";
+      this.isNavbarCollapsed = true;
+    } else {
+      // comment this line if you don't want to collapse the navbar when window is resized.
+      // this._isNavbarCollapsedAnim = 'closed';
+    }
+  }
+  toggleNavbar(): void {
+    if (this.isNavbarCollapsed) {
+      this._isNavbarCollapsedAnim = "open";
+      this.isNavbarCollapsed = false;
+    } else {
+      this._isNavbarCollapsedAnim = "closed";
+      this.isNavbarCollapsed = true;
+    }
+  }
+  get isNavbarCollapsedAnim(): string {
+    return this._isNavbarCollapsedAnim;
   }
 }
