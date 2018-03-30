991753eea14f493b96433a628f737e89c3c1db0e
Panagiotis Tsilopoulos <pan_tsilopoulos@outlook.com>
2018-03-31 01:24:32 +0300

    Add bubble chart in Dashboard

--- Web/ClientApp/src/app/dashboard/dashboard.component.ts
+++ Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -16,7 +16,7 @@ export class DashboardComponent {
   series: any[];
   multi: any[];
   view: any[] = [700, 400];
-  curve = d3.curveNatural;
+  curve = d3.curveMonotoneX;
   showXAxis = true;
   showYAxis = true;
   gradient = false;
@@ -37,6 +37,100 @@ export class DashboardComponent {
   colorScheme = {
     domain: ["#5AA454", "#A10A28", "#C7B42C", "#AAAAAA"]
   };
+  foo = [
+    {
+      "name": "Germany",
+      "series": [
+        {
+          "name": "2010",
+          "x": 40632,
+          "y": 80.3,
+          "r": 80.4
+        },
+        {
+          "name": "2000",
+          "x": 36953,
+          "y": 80.3,
+          "r": 78
+        },
+        {
+          "name": "1990",
+          "x": 31476,
+          "y": 75.4,
+          "r": 79
+        }
+      ]
+    },
+    {
+      "name": "USA",
+      "series": [
+        {
+          "name": "2010",
+          "x": 49737,
+          "y": 78.8,
+          "r": 310
+        },
+        {
+          "name": "2000",
+          "x": 45986,
+          "y": 76.9,
+          "r": 283
+        },
+        {
+          "name": "1990",
+          "x": 3706,
+          "y": 75.4,
+          "r": 253
+        }
+      ]
+    },
+    {
+      "name": "France",
+      "series": [
+        {
+          "name": "2010",
+          "x": 36745,
+          "y": 81.4,
+          "r": 63
+        },
+        {
+          "name": "2000",
+          "x": 34774,
+          "y": 79.1,
+          "r": 59.4
+        },
+        {
+          "name": "1990",
+          "x": 29476,
+          "y": 77.2,
+          "r": 56.9
+        }
+      ]
+    },
+    {
+      "name": "United Kingdom",
+      "series": [
+        {
+          "name": "2010",
+          "x": 36240,
+          "y": 80.2,
+          "r": 62.7
+        },
+        {
+          "name": "2000",
+          "x": 32543,
+          "y": 77.8,
+          "r": 58.9
+        },
+        {
+          "name": "1990",
+          "x": 26424,
+          "y": 75.7,
+          "r": 57.1
+        }
+      ]
+    }
+  ];
 
   constructor(private readonly glucoseService: GlucoseService,
     private readonly http: HttpClient,
@@ -45,7 +139,8 @@ export class DashboardComponent {
       http.get<IGlucose[]>(apiGatewayUrl + `api/glucose/${localStorage.getItem("userId")}`).subscribe(result => {
         const series = [{ "name": "BG", "series": this.transformToD3DataSeries(result) }];
         const multi = this.separateReferenceDataSeries(result);
-        Object.assign(this, { series, multi });
+        const boo = this.foo;
+        Object.assign(this, { series, multi, boo });
         this.dataLoaded = true;
       }, error => console.error(error));
   }
